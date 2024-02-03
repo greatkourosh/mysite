ddb8fa53a5490163cf26bdc6d167b5f0a421c690
Kourosh <bakhtiarifard@outlook.com>
2024-02-03 11:32:09 +0330

    Render HTML files Created
    Templates Folder and HTML Files Added

--- website/views.py
+++ website/views.py
@@ -1,18 +1,22 @@
 
 from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('<h1>Homepage<h1>')
+    # return HttpResponse('<h1>This is Home<h1>')
+    return render(request, 'website/index.html')
 
 def home(request):
     return index()
 
 def about(request):
-    return HttpResponse('<h1>This is about<h1>')
+    # return HttpResponse('<h1>This is about<h1>')
+    return render(request, 'website/about.html')
 
 def contact(request):
-    return HttpResponse('<h1>This is contact<h1>')
+    # return HttpResponse('<h1>This is contact<h1>')
+    return render(request, 'website/contact.html')
 
 def http_test(request):
     return HttpResponse('<h1>This is a test<h1>')
