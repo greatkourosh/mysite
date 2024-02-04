fd3ab0857aaef15ebb2b396a97c0b64d9511bc78
Kourosh <bakhtiarifard@outlook.com>
2024-02-04 11:44:02 +0330

    Travel Template Added
    Dynamic URLs Fixed, app_name 'website' Added, base.html Implemented

--- website/urls.py
+++ website/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.urls import path
 
 from website import views
-
+app_name = 'website'
 urlpatterns = [
     path("", views.index, name="index"),
     path("home", views.index, name="home"),
