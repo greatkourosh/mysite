8cbf081133d3d20f419494e56c77de4ba19545bf
Kourosh <bakhtiarifard@outlook.com>
2024-02-12 15:24:53 +0330

    Login, Logout Implemented

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 
 # Create your views here.
@@ -26,15 +27,20 @@ def login_view(request):
         return HttpResponseRedirect('/')
     else:
         if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return HttpResponseRedirect('/accounts')
-            else:
-                messages.error(
-                    request, 'User Not Found!')
+            # username = request.POST.get('username')
+            # password = request.POST.get('password')
+            # user = authenticate(request, username=username, password=password)
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get("username")
+                password = form.cleaned_data.get("password")
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return HttpResponseRedirect('/accounts')
+                else:
+                    messages.error(
+                        request, 'User Not Found!')
     # if request.user.is_authenticated:
     #     if_login = True
     #     user = request.user.username
@@ -43,7 +49,9 @@ def login_view(request):
     # else:
     #     if_login = False
     #     user = None
+    form = AuthenticationForm()
     context = {
+        'form': form,
         # "if_login": if_login,
         # "user": user,
     }
